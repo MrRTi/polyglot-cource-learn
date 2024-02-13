d9b2cbe0844c6a5342a5ab2ab6fbcdee15a5eb21
Artem Musalitin <musalitin.a.a@gmail.com>
2024-02-13 20:55:34 +0100

    nullable

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,13 @@
-enum RSEnum {
-    Foo2(Option<i32>),
-    Foo(i32),
-    Bar(String),
-    Baz(Vec<String>),
-    Baz2(fn() -> i32),
-}
-
-fn baz() -> i32 {
-    5
-}
-
 fn main() {
-    let foo = RSEnum::Foo(5);
+    let foo = Some(5);
 
-    if let RSEnum::Foo(value) = foo {}
-    if let RSEnum::Baz2(value) = foo {}
+    if let Some(value) = foo {}
 
     match foo {
-        RSEnum::Foo2(Some(value)) => {}
-        RSEnum::Foo2(None) => {}
-        RSEnum::Foo(value) => {}
+        Some(value) => {}
+        None => {}
     }
+
+    foo.map(|x| {});
+    foo.filter(|x| *x < 10);
 }
