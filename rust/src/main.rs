febb51a5b27efd433933bb802f47fb1b7f3325ca
Artem Musalitin <musalitin.a.a@gmail.com>
2024-02-13 20:41:47 +0100

    rust enums

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,24 @@
 enum RSEnum {
-  Foo,
-  Bar,
-  Baz,
+    Foo2(Option<i32>),
+    Foo(i32),
+    Bar(String),
+    Baz(Vec<String>),
+    Baz2(fn() -> i32),
+}
+
+fn baz() -> i32 {
+    5
 }
 
 fn main() {
+    let foo = RSEnum::Foo(5);
 
+    if let RSEnum::Foo(value) = foo {}
+    if let RSEnum::Baz2(value) = foo {}
+
+    match foo {
+        RSEnum::Foo2(Some(value)) => {}
+        RSEnum::Foo2(None) => {}
+        RSEnum::Foo(value) => {}
+    }
 }
