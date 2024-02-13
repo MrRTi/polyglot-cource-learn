d061898150d38b32e54b428057b29400d65bc8c0
Artem Musalitin <musalitin.a.a@gmail.com>
2024-02-13 20:35:13 +0100

    enums intro

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,9 @@
+enum RSEnum {
+  Foo,
+  Bar,
+  Baz,
+}
+
 fn main() {
-    let mut x = 5;
-    let z = &mut x;
-    *z = 7;
-    println!("{:?}", z);
+
 }
--- ts/test.js
+++
@@ -1,4 +0,0 @@
-const a = [];
-const b = a;
-b.push(1);
-console.log(a);
